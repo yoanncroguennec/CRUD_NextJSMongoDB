52a376a24fd6cb92d6925ae17e4b2ba9f8a37e48
sido69 <yoann.croguennec@gmail.com>
2024-07-02 22:22:45 +0200

    first commit

--- app/[action]/page.js
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-// // import AddProduct from "@/app/components/add-product";
-
-// const ActionProduct = () => {
-//   return <AddProduct />;
-// };
-
-// export default ActionProduct;
